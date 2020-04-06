41c5d047bd09eaf9649b13904176c54af5326596
ahamel1 <ahamel.wcs44@gmail.com>
2020-04-06 20:14:37 +0200

    modification de l'etat online offline

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,22 +2,35 @@ import React from 'react';
 import './Contact.css';
 import PropTypes from 'prop-types'
 
-const Contact = ({name, avatar, isOnline}) => {
-    return (
-        <div className = "Contact">
-            <div>
-                <img className="avatar" src ={avatar} alt = {name}/>
-            </div>
-            <div>
-                <h4 className="name">{name}</h4>
-                <div className="status">
-                    <div className={isOnline ?'status-online' : 'status-offline'}></div>
-                    <p className = "status-text">{isOnline ? "online" : "offline"}</p>   
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOnline : props.isOnline
+        }
+    }
+    render() {
+        return (
+            <div className = "Contact">
+                <div>
+                    <img className="avatar" src ={this.props.avatar} alt = {this.props.name}/>
                 </div>
-            </div>
+                <div>
+                    <h4 className="name">{this.props.name}</h4>
+                    <div className="status">
+                        <div className={this.state.isOnline ?'status-online' : 'status-offline'}
+                        onClick = {event => {
+                            const newIsOnline = !this.state.isOnline;
+                            this.setState({isOnline : newIsOnline});
+                        }}>
+                        </div>
+                        <p className = "status-text">{this.state.isOnline ? "online" : "offline"}</p>   
+                    </div>
+                </div>
 
-        </div>
-    );
+            </div>
+        );
+    }
 }
 Contact.propTypes = {
     name: PropTypes.string.isRequired,
